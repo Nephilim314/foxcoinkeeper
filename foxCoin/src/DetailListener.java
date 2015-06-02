ce78732ba44c05ca6d9cefdcb59a62b8f3d851a5
Nayeem Aquib <nayeemaquib@bennington.edu>
2015-06-02 06:26:44 -0400

    Create DetailListener.java

---
+++ foxCoin/src/DetailListener.java
@@ -0,0 +1,9 @@
+/**
+ * Created by DarkWizard on 6/1/15.
+ */
+
+import java.util.EventListener;
+
+public interface DetailListener extends EventListener {
+    public void detailEventOccurred(DetailEvent event);
+}
